4c24792a1b2558d579849b433ca92989fb4e71bb
koh <koh0617z3@gmail.comm>
2017-10-25 14:57:38 +0900

    made bars more thin

--- p1024/index.js
+++ p1024/index.js
@@ -10,14 +10,14 @@ function setup() {
 
 function draw() {
   background(255);
-  off +=0.09;
+  off +=0.03;
   drawSpectrum(300);
 }
 
 function drawSpectrum(size) {
   //var amplitude = size * (0.2 + 0.8 * (1 - 0.1));
-  var barWidth = size * 0.1;
-  var barInterval = size * 0.2;
+  var barWidth = size * 0.05;
+  var barInterval = size * 0.06;
   var halfSize = size * 0.5;
   var offsetXPosition = -halfSize + barWidth * 0.5;
   var ran = random(10);
@@ -25,7 +25,17 @@ function drawSpectrum(size) {
   rectMode(CORNER);
   noStroke();
   fill(0);
-  for (var i = 1; i < 10; i++) {
+  // for (var i = 1; i < 10; i++) {
+  //   // if you set i=0, the first bar can't move.
+  //   var barLength =  noise(off*i/45)*900;
+  //   rect(
+  //     100,
+  //     i * barInterval,
+  //     barLength - halfSize,
+  //     barWidth
+  //   )
+  // }
+  for (var i = 1; i < 30; i++) {
     // if you set i=0, the first bar can't move.
     var barLength =  noise(off*i/45)*900;
     rect(
